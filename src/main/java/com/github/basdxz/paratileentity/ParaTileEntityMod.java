0f479522b576ac9ab8c187055ae76bf15ebef2ab
basdxz <tudurap.com@gmail.com>
2021-10-20 22:53:19 +0100

    More quick branding - I should streamline this...

--- src/main/java/com/github/basdxz/paratileentity/ParaTileEntityMod.java
+++ src/main/java/com/github/basdxz/paratileentity/ParaTileEntityMod.java
@@ -8,7 +8,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 @Mod(modid = ParaTileEntityMod.MODID, name = ParaTileEntityMod.NAME, version = ParaTileEntityMod.VERSION)
 public class ParaTileEntityMod {
     public static final String MODID = "paratileentity";
-    public static final String NAME = "Many In One";
+    public static final String NAME = "ParaTileEntity";
     public static final String VERSION = "@GRADLE_VERSION_TOKEN@";
 
     @EventHandler
