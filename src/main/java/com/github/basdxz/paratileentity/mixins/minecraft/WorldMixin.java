c4d31511f508d7880b92f5dc655a37494e38479a
basdxz <tudurap.com@gmail.com>
2021-11-09 02:03:33 +0000

    WHOPS, YEETED A CHECK

--- src/main/java/com/github/basdxz/paratileentity/mixins/minecraft/WorldMixin.java
+++ src/main/java/com/github/basdxz/paratileentity/mixins/minecraft/WorldMixin.java
@@ -17,12 +17,17 @@ public class WorldMixin {
 
     /*
         Buffers a ParaTile a new IParaTileEntity is about to be created.
+
+        A meta 0 will be called on a multi block update much more frequently than someone placing down a null tile.
      */
     @Inject(method = "setBlock(IIILnet/minecraft/block/Block;II)Z",
             at = @At("HEAD"),
             require = 1)
     public void setBlock(int posX, int posY, int posZ, Block block, int meta, int flag,
                          CallbackInfoReturnable<Boolean> cir) {
+        if (!(block instanceof IParaBlock))
+            return;
+
         val tileEntity = Utils.getTileEntityIfExists(thiz(), posX, posY, posZ);
         if (!tileEntity.isPresent() ||
                 !(tileEntity.get() instanceof IParaTileEntity) || ((IParaTileEntity) tileEntity.get()).tileID() != meta)
