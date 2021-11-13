1e00379edd7c4896ec337560f13a35746f4b31b3
basdxz <tudurap.com@gmail.com>
2021-11-13 12:48:22 +0000

    Moved injection point

--- src/main/java/com/github/basdxz/paratileentity/mixins/minecraft/EntityRendererMixin.java
+++ src/main/java/com/github/basdxz/paratileentity/mixins/minecraft/EntityRendererMixin.java
@@ -14,11 +14,12 @@ public class EntityRendererMixin {
 
     @Inject(method = "orientCamera(F)V",
             at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/client/renderer/RenderGlobal;hasCloudFog (DDDF)Z"),
+                    target = "Lnet/minecraft/util/MathHelper;sin (F)F",
+                    ordinal = 1,
+                    by = 2),
             locals = LocalCapture.CAPTURE_FAILEXCEPTION,
             require = 1)
-    private void renderParticlesInject(float f6, CallbackInfo ci, EntityLivingBase entitylivingbase, float f1,
-                                       double d0, double d1, double d2) {
+    private void renderParticlesInject(float f6, CallbackInfo ci, EntityLivingBase entitylivingbase, float f1, double d0, double d1, double d2, double d7, float f2, float f6_a, double d3, double d4) {
         StaticStorage.camPosX = d0;
         StaticStorage.camPosY = d1;
         StaticStorage.camPosZ = d2;
