2618ecff10895b7f375a35545f862734594b0dfb
basdxz <tudurap.com@gmail.com>
2021-11-13 11:17:29 +0000

    Gibbeth-

--- src/main/java/com/github/basdxz/paratileentity/mixins/minecraft/EffectRendererMixin.java
+++ src/main/java/com/github/basdxz/paratileentity/mixins/minecraft/EffectRendererMixin.java
@@ -2,14 +2,24 @@ package com.github.basdxz.paratileentity.mixins.minecraft;
 
 import com.github.basdxz.paratileentity.util.Utils;
 import net.minecraft.client.particle.EffectRenderer;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.entity.Entity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.List;
 
 // Client-Side
 @Mixin(EffectRenderer.class)
 public class EffectRendererMixin {
+    @Shadow
+    private List[] fxLayers;
 
     /*
         Replaces block meta with IParaTile tileID if block is an instance of IParaBlock.
@@ -22,5 +32,18 @@ public class EffectRendererMixin {
             require = 1)
     private int getBlockMetadataRedirect(World instance, int posX, int posY, int posZ) {
         return Utils.getBlockMetadataRedirect(instance, posX, posY, posZ);
+    }
+
+    @Inject(method = "renderParticles(Lnet/minecraft/entity/Entity;F)V",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/client/renderer/Tessellator;startDrawingQuads ()V"),
+            locals = LocalCapture.CAPTURE_FAILEXCEPTION,
+            require = 1)
+    private void renderParticlesInject(Entity p_78874_1_, float p_78874_2_, CallbackInfo ci,
+                                       float f1, float f2, float f3, float f4, float f5, int k, int i,
+                                       Tessellator tessellator) {
+        for (List fxLayer : fxLayers) {
+            System.out.println(fxLayer);
+        }
     }
 }
