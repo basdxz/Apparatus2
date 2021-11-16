2ae71e4ebf8740b88e4fd4c812841d13a39d4e85
basdxz <tudurap.com@gmail.com>
2021-11-16 05:58:06 +0000

    Remove old NBT code

--- src/main/java/com/github/basdxz/paratileentity/defenition/managed/ParaTileEntityBase.java
+++ src/main/java/com/github/basdxz/paratileentity/defenition/managed/ParaTileEntityBase.java
@@ -138,15 +138,6 @@ public abstract class ParaTileEntityBase extends TileEntity implements IParaTile
     @Override
     public void readFromNBT(NBTTagCompound nbtTagCompound) {
         super.readFromNBT(nbtTagCompound);
-        //val tileIDFromNBT = readTileIDFromNBT(nbtTagCompound);
-        //if (tileID() != tileIDFromNBT) {
-        //    ParaTileEntityMod.debug("Paratile Desync, should reconstruct & rebind!");
-        //    manager().bufferedTile(worldObj(), posX(), posY(), posZ(), tileIDFromNBT);
-        //    val tileEntity = createNewTileEntity();
-        //    worldObj().setTileEntity(posX(), posY(), posZ(), tileEntity);
-        //    ((IParaTileEntity) tileEntity).paraTile().updateBlock();
-        //    tileEntity.readFromNBT(nbtTagCompound);
-        //}
         if (!paraTile.singleton())
             paraTile.readFromNBT(nbtTagCompound);
     }
