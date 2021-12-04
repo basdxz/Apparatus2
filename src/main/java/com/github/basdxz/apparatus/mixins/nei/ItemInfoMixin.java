53f55ca994ded227227cde9a83cc1c05d787ee56
basdxz <tudurap.com@gmail.com>
2021-12-04 21:29:51 +0000

    Wierdest remap issue??

--- src/main/java/com/github/basdxz/apparatus/mixins/nei/ItemInfoMixin.java
+++ src/main/java/com/github/basdxz/apparatus/mixins/nei/ItemInfoMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = ItemInfo.class, remap = false)
+@Mixin(ItemInfo.class)
 public class ItemInfoMixin {
     @Redirect(method = "getIdentifierItems(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;" +
             "Lnet/minecraft/util/MovingObjectPosition;)Ljava/util/ArrayList;",
@@ -19,7 +19,8 @@ public class ItemInfoMixin {
                     target = "Lnet/minecraft/block/Block;getPickBlock " +
                             "(Lnet/minecraft/util/MovingObjectPosition;Lnet/minecraft/world/World;" +
                             "IIILnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;"),
-            require = 1)
+            require = 1,
+            remap = false)
     private static ItemStack getPickBlockRedirect(Block instance, MovingObjectPosition hit,
                                                   World world, int posX, int posY, int posZ, EntityPlayer player) {
         return Utils.bufferParaTile(instance.getPickBlock(hit, world, posX, posY, posZ, player));
